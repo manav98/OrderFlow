c255133e662445dab2f72ae8ed8b398399bb77c5
Manav Gupta <37526340+manav98@users.noreply.github.com>
2026-08-23 00:40:02 +0530

    initiales repo

---
+++ src/main/java/com/example/OrderFlow/OrderFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.OrderFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFlowApplication.class, args);
+	}
+
+}
